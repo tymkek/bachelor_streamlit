51e5cd1dbb8baab1791fac80d9c8ad9d6efef488
tymkek <151462833+tymkek@users.noreply.github.com>
2024-05-10 14:51:29 +0200

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -8,8 +8,10 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import certifi
+from itertools import islice
 
 load_dotenv()
+
 
 client = MongoClient(st.secrets["db_uri"], tlsCAFile=certifi.where())
 db = client["bacluster"]
@@ -24,39 +26,41 @@ class SurveySite:
             self.t_books = file.read()
         with open("./config/sigma.txt", "r") as file:
             self.t_sigma = file.read()
-
+        
     def set_state(self, state):
         st.session_state.page = state
-
-    def set_questions(self, filename):
-        with open(filename, "r") as file:
-            q = json.load(file)
-        self.questions = q
     
-    def db_callback(self, file, state ):
-        db.ans.insert_one(file)
+    def db_callback(self, file, state, collection):
+        insert_result = db[collection].insert_one(file)
+        st.session_state.file_id = insert_result.inserted_id
         self.set_state(state)
-        
+    
     def question_page(self, questions):
+        
+        st.session_state.questions_id = st.session_state.page
         with st.container():
             ans_dict = {}
+            ans_dict["questions_id"] = st.session_state.page
+            
             for q in questions["questions"]:
                 ans = st.text_area(q["question"])
                 if ans:
                     ans_dict[q["question"]] = ans
             
-            st.button("Submit answers", on_click=self.db_callback, args=[ans_dict, "final"])
+            st.button("Submit answers", on_click=self.db_callback, args=[ans_dict, "rate_page", "ans"])
 
 
     def home_page(self):
         with st.container():
-            st.title("Experiment")
-            st.write("You will be given 4 texts to choose from")
-            st.write("After choosing one, you will have 5 minutes to read it")
+            st.title("Evaluation of user knowledge with LLM and human feedback - experiment")
+            st.write("Experiment is conducted in a survey form, consisting of two parts. Completing the test will take you approximately **7 minutes**.")
             st.write(
-                "In the last stage, 5 question will be generated to chosen text, answer them as best as you can"
+                "In the first part, you will be choosing a short article to read. You will have 5 minutes to do so, with option to proceed further, regardless of the remaining time. After that you will be shown five questions based on the chosen text, answer them **accordingly to your knowledge**."
             )
-            st.write("By pressing the button, you agree to participate in experiment")
+            st.write(
+                "In the second part, you will be evaluating 5 different answers to the same questions set, with given evaluation scale."
+            )
+            st.write("Participation is anonymous, by pressing the button, you agree to the experiment terms.")
             st.button("Next", on_click=self.set_state, args=["choose"])
 
     def final_page(self):
@@ -65,22 +69,27 @@ class SurveySite:
 
     def choose_page(self):
         with st.container():
-            tab1, tab2, tab3, tab4 = st.tabs(["Text 1", "Text 2", "Text 3", "Text 4"])
+            st.title("Please, choose the article you would like to read")
+            tab1, tab2, tab3, tab4 = st.tabs(["Article 1", "Article 2", "Article 3", "Article 4"])
             with tab1:
                 st.header("The truth about sigma males")
                 st.write("A tale of toxic masculinity and romanticising loneliness")
+                st.write("Genere: pop culture, social media")
                 st.button("Choose", key="t1", on_click=self.set_state, args=["text1"])
 
             with tab2:
                 st.header("Reading Books Is Useless: Here's a Better Way to Read")
+                st.write("Genere: studying, ")
                 st.button("Choose", key="t2", on_click=self.set_state, args=["text2"])
 
             with tab3:
-                st.header("text 3")
+                st.header("Article 3 placeholder, choosing it will redirect you to article 1")
+                st.write("Genere: science")
                 st.button("Choose", key="t3", on_click=self.set_state, args=["text1"])
 
             with tab4:
-                st.header("text 4")
+                st.header("Article 4 placeholder, choosing it will redirect you to article 1")
+                st.write("Genere: politics")
                 st.button("Choose", key="t4", on_click=self.set_state, args=["text1"])
 
     def text_page(self, text):
@@ -114,6 +123,43 @@ class SurveySite:
             timer.write("")
             article.write("Time ended, please proceed further")
 
+    def rate_page(self):
+
+        collection = db['ans']
+
+        filter_criteria = {
+             'questions_id': st.session_state.questions_id 
+        }
+        pipeline = [
+            {
+            '$match': {
+                '_id': {'$ne': st.session_state.file_id},
+                **filter_criteria
+            }
+        },
+            {
+            '$sample': {'size': 1}  
+        }
+        ]
+        result = list(collection.aggregate(pipeline))
+        selected_options = {}
+        selected_options["questions_id"] = st.session_state.questions_id
+        with st.container():
+            for key, value in islice(result[0].items(), 2, None):
+    
+                st.subheader(key)
+                st.write(value)
+    
+                choice = st.radio(
+                    f"Choose an option related to {key}:",
+                    ('very bad', 'bad', 'average', 'good', 'excellent'),
+                    key=key
+                )
+                selected_options[key] = {"ans": value, "eval": choice}
+
+            print(selected_options)
+            st.button("Submit", on_click=self.db_callback, args=[selected_options, "final", "eval"])
+
     def main(self):
 
         if "page" not in st.session_state:
@@ -142,6 +188,9 @@ class SurveySite:
 
                 case "text2":
                     self.text_page(self.t_books)
+                
+                case "rate_page":
+                    self.rate_page()
 
                 case "final":
                     self.final_page()
